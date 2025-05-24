a2861797bac6ef6cc12a50242271159423c56f5f
Ecodrin <matvfo@ya.ru>
2025-05-24 19:23:34 +0300

    KAN-20 | feature(rl): fixed conflicts

--- rl/environment/__init__.py
+++
@@ -1,7 +0,0 @@
-from gymnasium.envs.registration import register
-
-register(
-    id='CarrierRobot-v0',
-    entry_point='environment.carrier_robot_gym:CarrierRobotEnv',
-    kwargs={"width": 10, "height": 10, "render_mode": "human"}
-)
